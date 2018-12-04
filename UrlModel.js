c5e28454ac8d9dff5973096ea58c04af183f808a
Bhumesh Domala <umeshgmrl@gmail.com>
2018-12-05 00:49:28 +0530

    implemented the functionality

--- UrlModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const urlSchema = new Schema({
-	originalUrl: String,
-	shortenedUrl: String
-});
-
-mongoose.model("urls", urlSchema);
--- server.js
+++ server.js
@@ -3,24 +3,29 @@
 var express = require("express");
 var mongo = require("mongodb");
 var mongoose = require("mongoose");
+const { Schema } = mongoose;
 var cors = require("cors");
 var app = express();
-require("./UrlModel");
-const URL = mongoose.model("urls");
+var bodyParser = require("body-parser");
 
-mongoose.connect("");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Basic Configuration
+const urlSchema = new Schema({
+	originalUrl: String,
+	index: Number
+});
+
+const URL = mongoose.model("URL", urlSchema);
+
+mongoose.connect(
+	process.env.MONGO_URI,
+	() => console.log("connected")
+);
+
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/
-
-// mongoose.connect(process.env.MONGOLAB_URI);
-
 app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
@@ -29,8 +34,30 @@ app.get("/", function(req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/shorturl/:url", function(req, res) {
-	res.json({ url: req.params.url });
+app.post("/api/shorturl/new", async function(req, res) {
+	const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+	if (!urlRegex.test(req.body.url)) return res.json({ error: "invalid URL" });
+	const url = await URL.findOne({ originalUrl: req.body.url });
+	if (url) {
+		return res.json({ original_url: url.originalUrl, short_url: url.index });
+	} else {
+		const index = (await URL.count()) + 1;
+		const url = new URL({
+			originalUrl: req.body.url,
+			index
+		});
+		url
+			.save()
+			.then(doc =>
+				res.json({ original_url: doc.originalUrl, short_url: doc.index })
+			);
+	}
+});
+
+app.get("/api/shorturl/:index", async function(req, res) {
+	const index = parseInt(req.params.index);
+	const url = await URL.findOne({ index });
+	res.redirect(url.originalUrl);
 });
 
 app.listen(port, function() {
